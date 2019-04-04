66de36a4ddb010748cb880eda001e02a5e64f26e
Oliver <o.dzineit@gmail.com>
2019-04-04 21:44:32 +0100

    Port all events which had been added in Spout version over to Bukkit format

---
+++ src/main/java/pw/ollie/politics/event/group/GroupCreateEvent.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of Politics.
+ *
+ * Copyright (c) 2019 Oliver Stanley
+ * Politics is licensed under the Affero General Public License Version 3.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package pw.ollie.politics.event.group;
+
+import pw.ollie.politics.group.Group;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public final class GroupCreateEvent extends GroupEvent implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+
+    private final CommandSender creator;
+
+    private boolean cancelled;
+
+    public GroupCreateEvent(Group group, CommandSender creator) {
+        super(group);
+        this.creator = creator;
+    }
+
+    public CommandSender getCreator() {
+        return creator;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
