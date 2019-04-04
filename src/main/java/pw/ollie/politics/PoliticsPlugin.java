21dd8883473979e52e0fecf91405b2af1d80f1b4
Oliver <o.dzineit@gmail.com>
2019-04-04 10:46:28 +0100

    Create key files

---
+++ src/main/java/pw/ollie/politics/PoliticsPlugin.java
@@ -0,0 +1,13 @@
+package pw.ollie.politics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PoliticsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
