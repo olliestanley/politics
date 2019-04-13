0eb9c4dcee592462a82c0d1acc094e7c9b3acd5b
Oliver <o.dzineit@gmail.com>
2019-04-13 20:58:06 +0100

    Check group isn't open before allowing invite sending

--- src/main/java/pw/ollie/politics/command/group/GroupInviteCommand.java
+++ src/main/java/pw/ollie/politics/command/group/GroupInviteCommand.java
@@ -23,6 +23,7 @@ import pw.ollie.politics.PoliticsPlugin;
 import pw.ollie.politics.command.CommandException;
 import pw.ollie.politics.command.args.Arguments;
 import pw.ollie.politics.group.Group;
+import pw.ollie.politics.group.GroupProperty;
 import pw.ollie.politics.group.level.GroupLevel;
 import pw.ollie.politics.group.privilege.Privileges;
 
@@ -46,6 +47,10 @@ public class GroupInviteCommand extends GroupSubCommand {
 
         if (args.length(false) < 1) {
             throw new CommandException("There was no player specified to invite.");
+        }
+
+        if (group.getBooleanProperty(GroupProperty.OPEN, false)) {
+            throw new CommandException("The " + groupLevel.getName() + " is open to join without invitation.");
         }
 
         Player player = plugin.getServer().getPlayer(args.getString(0, false));
