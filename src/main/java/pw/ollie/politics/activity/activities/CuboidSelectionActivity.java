794a153523f5116562ccd09343da3bce20885515
Oliver <o.dzineit@gmail.com>
2019-04-19 22:05:40 +0100

    Add cuboid selection activity

---
+++ src/main/java/pw/ollie/politics/activity/activities/CuboidSelectionActivity.java
@@ -0,0 +1,88 @@
+/*
+ * This file is part of Politics.
+ *
+ * Copyright (c) 2019 Oliver Stanley
+ * Politics is licensed under the Affero General Public License Version 3.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package pw.ollie.politics.activity.activities;
+
+import pw.ollie.politics.activity.PoliticsActivity;
+import pw.ollie.politics.util.Position;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+public class CuboidSelectionActivity implements PoliticsActivity {
+    private final UUID playerId;
+    private final Consumer<CuboidSelectionActivity> callback;
+
+    private Position firstPoint = null;
+    private Position secondPoint = null;
+
+    private boolean completed = false;
+
+    public CuboidSelectionActivity(UUID playerId, Consumer<CuboidSelectionActivity> callback) {
+        this.playerId = playerId;
+        this.callback = callback;
+    }
+
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public Position getFirstPoint() {
+        return firstPoint;
+    }
+
+    public void setFirstPoint(Position firstPoint) {
+        this.firstPoint = firstPoint;
+    }
+
+    public boolean isFirstPointSet() {
+        return firstPoint != null;
+    }
+
+    public Position getSecondPoint() {
+        return secondPoint;
+    }
+
+    public void setSecondPoint(Position secondPoint) {
+        this.secondPoint = secondPoint;
+    }
+
+    public boolean isSecondPointSet() {
+        return secondPoint != null;
+    }
+
+    public boolean hasCompleted() {
+        return completed;
+    }
+
+    public boolean complete() {
+        if (completed || !isFirstPointSet() || !isSecondPointSet()) {
+            return false;
+        }
+
+        callback.accept(this);
+        completed = true;
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Cuboid Selection";
+    }
+}
