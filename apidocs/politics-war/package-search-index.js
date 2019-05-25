db74454b2187c8ed55a4ffb51a60b811e9284b7c
Oliver <o.dzineit@gmail.com>
2019-05-25 09:57:52 +0100

    Update to new apidocs for politics-core and politics-war separately

---
+++ apidocs/politics-war/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"pw.ollie.politicswar"},{"l":"pw.ollie.politicswar.command"},{"l":"pw.ollie.politicswar.event"},{"l":"pw.ollie.politicswar.event.war"},{"l":"pw.ollie.politicswar.war"}]
